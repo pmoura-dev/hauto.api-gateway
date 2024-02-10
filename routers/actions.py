66479b699ed671964b9d23eaa5875de27542067d
Pedro Moura <pmoura.dev@gmail.com>
2024-02-10 15:21:00 +0000

    add endpoints for air conditioners

--- routers/actions.py
+++ routers/actions.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from errors.models import HAUTOError
 from services import action
@@ -29,3 +30,38 @@ async def turn_off(device_id: int):
 @router.post("/set_rgb_color")
 async def set_rgb_color(device_id: int, body: dict):
     raise HAUTOError(501, "Action is not implemented")
+
+
+class Temperature(BaseModel):
+    value: int
+
+
+@router.post("/set_heating_threshold_temperature")
+async def set_heating_threshold_temperature(device_id: int, temperature: Temperature):
+    try:
+        await action.set_heating_threshold_temperature(device_id, temperature.value)
+    except Exception as e:
+        print(e)
+        raise HAUTOError(501, "Action is not implemented")
+
+
+@router.post("/set_cooling_threshold_temperature")
+async def set_cooling_threshold_temperature(device_id: int, temperature: Temperature):
+    try:
+        await action.set_cooling_threshold_temperature(device_id, temperature.value)
+    except Exception as e:
+        print(e)
+        raise HAUTOError(501, "Action is not implemented")
+
+
+class HeaterCoolerMode(BaseModel):
+    mode: str
+
+
+@router.post("/set_heater_cooler_mode")
+async def set_rgb_color(device_id: int, mode: HeaterCoolerMode):
+    try:
+        await action.set_heater_cooler_mode(device_id, mode.mode)
+    except Exception as e:
+        print(e)
+        raise HAUTOError(501, "Action is not implemented")
--- services/action.py
+++ services/action.py
@@ -9,3 +9,16 @@ async def turn_on(device_id: int):
 async def turn_off(device_id: int):
     message = {"device_id": device_id}
     await action_request("turn_off", message)
+
+
+async def set_heating_threshold_temperature(device_id: int, value: int):
+    message = {"device_id": device_id, "value": value}
+    await action_request("set_heating_threshold_temperature", message)
+
+async def set_cooling_threshold_temperature(device_id: int, value: int):
+    message = {"device_id": device_id, "value": value}
+    await action_request("set_cooling_threshold_temperature", message)
+
+async def set_heater_cooler_mode(device_id: int, mode: str):
+    message = {"device_id": device_id, "mode": mode}
+    await action_request("set_heater_cooler_mode", message)
