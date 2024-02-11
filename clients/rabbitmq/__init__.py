558efdd7b7b015b2c4169493eaf0f2c8fd7e7d9b
Pedro Moura <pmoura.dev@gmail.com>
2024-02-11 17:49:42 +0000

    ci cd

--- clients/rabbitmq/__init__.py
+++ clients/rabbitmq/__init__.py
@@ -2,15 +2,22 @@ import os
 
 from faststream.rabbit import RabbitBroker
 
+import config
+
 
 class Client:
 
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, host, port, user, password):
+        self.url = f'amqp://{user}:{password}@{host}:{port}'
 
     async def publish(self, message: dict, topic: str, exchange: str):
         async with RabbitBroker(self.url) as broker:
             await broker.publish(message=message, exchange=exchange, routing_key=topic)
 
 
-client = Client(os.environ.get("rabbitmq_host"))
+client = Client(
+    host=config.RABBITMQ_HOST,
+    port=config.RABBITMQ_PORT,
+    user=config.RABBITMQ_USER,
+    password=config.RABBITMQ_PASSWORD
+)
--- clients/transaction_service/__init__.py
+++ clients/transaction_service/__init__.py
@@ -3,14 +3,19 @@ from typing import Dict, Any
 
 import requests
 
+import config
+
 
 class Client:
 
-    def __init__(self, base_url):
-        self.base_url = base_url
+    def __init__(self, host, port):
+        self.base_url = f"http://{host}:{port}"
 
     def get(self, path: str, params: Dict[str, Any] | None = None) -> requests.Response:
         return requests.get(self.base_url + path, params=params)
 
 
-client = Client(os.environ.get("transaction_service_host"))
+client = Client(
+    host=config.TRANSACTION_SERVICE_HOST,
+    port=config.TRANSACTION_SERVICE_PORT
+)
